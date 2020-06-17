64e49e907f1a12ef584eb2408530f83a04ea9f1d
kelly <kelly.gang@campaigntocash.com>
2020-06-16 19:46:40 -0700

    google ads create page - case

--- src/app/pages/advertising/google-ads/google-ads-create/google-ads-create.component.ts
+++ src/app/pages/advertising/google-ads/google-ads-create/google-ads-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-google-ads-create',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoogleAdsCreateComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+
+  siteLinkCreateForm: FormGroup;
+  previewCreateForm: FormGroup;
+
+  siteLinksFormArray: FormArray;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      details: this.fb.group({
+        name: ['', Validators.required]
+      }),
+      locationAndDelivery: this.fb.group({}),
+      siteLinks: this.fb.array([]),
+      template: this.fb.group({
+        name: ['', Validators.required],
+        billingAccount: [''],
+        tagName: ['', Validators.required]
+      }),
+      groupDetail: this.fb.group({}),
+      adsPreview: this.fb.array([])
+    });
+
+    this.siteLinksFormArray = this.formGroup.controls.siteLinks as FormArray;
+
+    this.siteLinkCreateForm = this.fb.group({
+      text: ['', Validators.required],
+      description1: ['', Validators.required],
+      description2: [''],
+      finalUrl: ['', [Validators.required]]
+    });
+
+    this.previewCreateForm = this.fb.group({
+      headline1: ['', Validators.required],
+      headline2: ['', Validators.required],
+      description: ['', Validators.required]
+    });
   }
 
+  save() {
+    console.log('GoogleAdsCreate.Save => ', this.formGroup.value);
+  }
+
+  createNewSiteLink() {
+    const siteData = this.siteLinkCreateForm.getRawValue();
+    this.siteLinkCreateForm.setValue({
+      text: '', description1: '', description2: '', finalUrl: ''
+    });
+
+    this.siteLinksFormArray.push(this.fb.group({
+      text: siteData.text || '',
+      description1: siteData.description1 || '',
+      description2: siteData.description2 || '',
+      finalUrl: siteData.finalUrl || '',
+      id: uuidv4(),
+      check: true
+    }));
+  }
 }
--- src/app/pages/advertising/google-ads/google-ads.module.ts
+++ src/app/pages/advertising/google-ads/google-ads.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { GoogleAdsRoutingModule } from './google-ads-routing.module';
 import { GoogleAdsComponent } from './google-ads/google-ads.component';
 import { GoogleAdsCreateComponent } from './google-ads-create/google-ads-create.component';
 
-
 @NgModule({
   declarations: [GoogleAdsComponent, GoogleAdsCreateComponent],
   imports: [
     CommonModule,
-    GoogleAdsRoutingModule
+    GoogleAdsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class GoogleAdsModule { }
