14d13f85cfdf17011603174da3f3233b0a8604c8
Gabriel Chen <gabriel.chen@campaigntocash.com>
2020-06-26 11:40:50 +0200

    fix lint error of shared components

--- src/app/theme/shared/components/email-campaign-editor/directives/email-campaign-dynamics.directive.ts
+++ src/app/theme/shared/components/email-campaign-editor/directives/email-campaign-dynamics.directive.ts
@@ -1,26 +1,37 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { Directive, HostListener, Input, OnDestroy } from '@angular/core';
 
 import { ModalService } from '@app-components/modal/modal.service';
 import { DynamicConditionModalComponent } from '../components/dynamic-condition-modal/dynamic-condition-modal.component';
 
 import { EmailCampaignEditorComponent } from '@app-components/email-campaign-editor/email-campaign-editor.component';
 import { MobileCampaignEditorComponent } from '@app-components/email-campaign-editor/mobile-campaign-editor.component';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Directive({
   selector: '[appEmailCampaignDynamics]'
 })
-export class EmailCampaignDynamicsDirective {
+export class EmailCampaignDynamicsDirective implements OnDestroy {
   @Input() editor: EmailCampaignEditorComponent | MobileCampaignEditorComponent;
+
+  private unsubscribe$ = new Subject();
 
   constructor(
     private modalService: ModalService
   ) { }
 
   @HostListener('click') onClick() {
-    this.modalService.openModal(DynamicConditionModalComponent).afterClosedWithSuccess().subscribe(result => {
-      if (result && this.editor) {
-        this.editor.addCustomItem(result);
-      }
-    });
+    this.modalService.openModal(DynamicConditionModalComponent).afterClosedWithSuccess()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(result => {
+        if (result && this.editor) {
+          this.editor.addCustomItem(result);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
