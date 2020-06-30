7247d2bbff19723cd9136c01f36408ce59490273
kelly <kelly.gang@campaigntocash.com>
2020-07-01 00:41:17 +1000

    add usage-dashboard-page

---
+++ src/app/core/enums/usage-dashboard-type.enum.ts
@@ -0,0 +1,7 @@
+export enum  UsageViewOptionType{
+  Today = 'Today',
+  CurrentWeek = 'Current Week',
+  CurrentMonth = 'Current Month',
+  CurrentYear = 'Current Year',
+  PreviousYear = 'Previous Year'
+}
--- src/app/pages/setup/usage-dashboard/usage-dashboard.component.ts
+++ src/app/pages/setup/usage-dashboard/usage-dashboard.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { NgSelectData } from '@app-core/models/common';
+import { UsageViewOpion } from '@app-core/models/usage-dashboard';
+import { UsageViewOptionType } from '@app-core/enums/usage-dashboard-type.enum';
+import { DataTableSource, DataTableColumn } from '@app-components/datatable/datatable-source';
+import { OrganizationData } from '@app-core/models/usage-dashboard';
+import { ModalService } from '@app-components/modal/modal.service';
+import { UsageDashboardService } from '@app-core/services/usage-dashboard-service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+class AllUsageField {
+  field: string;
+  value: number;
+}
 
 @Component({
   selector: 'app-usage-dashboard',
@@ -7,18 +19,79 @@ import { NgSelectData } from '@app-core/models/common';
   styleUrls: ['./usage-dashboard.component.scss']
 })
 export class UsageDashboardComponent implements OnInit {
-  analyticItems: NgSelectData[] = [
-    {value: 'email', label: 'Email Analytics'},
-    {value: 'mobile', label: 'Mobile Analytics'},
-    {value: 'social', label: 'Social & PPC Analytics'},
-    {value: 'website', label: 'Website Analytics'},
-    {value: 'event', label: 'Event Analytics'},
-    {value: 'qrcode', label: 'QRCode Analytics'},
-    {value: 'lead', label: 'Lead Analytics'}
-  ]
-  constructor() { }
+
+  viewOptions: UsageViewOpion[] = [
+    { id: 'today', value: UsageViewOptionType.Today },
+    { id: 'currentWeek', value: UsageViewOptionType.CurrentWeek },
+    { id: 'currentMonth', value: UsageViewOptionType.CurrentMonth },
+    { id: 'currentYear', value: UsageViewOptionType.CurrentYear },
+    { id: 'previousYear', value: UsageViewOptionType.PreviousYear }
+  ];
+
+  allUsageFields: AllUsageField[] = [
+    {field: 'Total Number of Emails', value: 0},
+    {field: 'Total Number of Records', value: 0},
+    {field: 'Total Number of Users', value: 0},
+    {field: 'DB Usage', value: 0}
+  ];
+
+  tableSource: DataTableSource<OrganizationData> = new DataTableSource<OrganizationData>(10);
+  tableButtons = [];
+
+  allOrganizationData: OrganizationData[];
+  destroy$ = new Subject();
+
+  constructor(
+    private usageDashboardService: UsageDashboardService,
+    private modalService: ModalService
+  ) { }
 
   ngOnInit(): void {
+    this.usageDashboardService.getAllOrganizationMockData()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        data => {
+          this.allOrganizationData = data;
+        },
+        error => {
+          console.log('error', error);
+        }
+      )
+
+    this.tableSource.next(this.allOrganizationData.slice(0, 50), this.allOrganizationData.length);
+
+    this.tableSource.changed$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(change => {
+        let mockData = [];
+        if (change.search) {
+          mockData = this.allOrganizationData.filter(item => item.name.includes(change.search));
+        } else {
+          mockData = this.allOrganizationData;
+        }
+
+        this.tableSource.next(
+          mockData.slice(
+            change.pagination.pageSize * (change.pagination.pageNumber - 1),
+            change.pagination.pageSize * (change.pagination.pageNumber)),
+          mockData.length
+        );
+      });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    const columns: DataTableColumn[] = [
+      { name: 'Name', prop: 'name', sortable: true},
+      { name: 'Record Count', prop: 'recordCount', sortable: true},
+      { name: 'Sent Email Count', prop: 'sentEmailCount', sortable: true},
+      { name: 'User Count', prop: 'sentEmailCount', sortable: true},
+      { name: 'Disk Space', prop: 'sentEmailCount', sortable: true},
+    ];
+    this.tableSource.setColumns(columns);
+  }
 }
